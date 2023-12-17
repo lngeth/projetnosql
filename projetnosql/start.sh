34008da88064368495e1a7c0f769f4bbb8a02718
Laurent Ngeth <l.ngeth@yahoo.fr>
2023-12-17 22:20:48 +0100

    remove migration

--- projetnosql/start.sh
+++ projetnosql/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-python manage.py makemigrations projetnosql --no-input && python manage.py migrate --fake projetnosql zero && python manage.py migrate projetnosql --no-input && python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
